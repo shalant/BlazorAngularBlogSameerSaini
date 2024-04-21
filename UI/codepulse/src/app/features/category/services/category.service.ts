5f875ef415f75575af94da1288f98c19f4230a24
shalant <doug.rosenberg@gmail.com>
2024-04-20 21:51:32 -0500

    category in progress

--- UI/codepulse/src/app/features/category/services/category.service.ts
+++ UI/codepulse/src/app/features/category/services/category.service.ts
@@ -3,6 +3,8 @@ import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Category } from '../models/category.model';
+import { environment } from 'src/environments/environment';
+import { UpdateCategoryRequest } from '../models/update-category-request.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,10 +14,18 @@ export class CategoryService {
   constructor(private http: HttpClient) {}
 
     addCategory(model: AddCategoryRequest): Observable<void> {
-      return this.http.post<void>('https://localhost:7265/api/categories', model);
+      return this.http.post<void>(`${environment.apiBaseUrl}/api/categories`, model);
     }
    
     getAllCategories(): Observable<Category[]> {
-      return this.http.get<Category[]>('https://localhost:7265/api/categories');
+      return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/categories`);
+    }
+
+    getCategoryById(id: string): Observable<Category> {
+      return this.http.get<Category>(`${environment.apiBaseUrl}/api/categories/${id}`)
+    }
+
+    updateCategory(id: string, updateCategoryRequest: UpdateCategoryRequest) : Observable<Category> {
+      return this.http.put<Category>(`${environment.apiBaseUrl}/api/categories/${id}`, updateCategoryRequest)
     }
 }
